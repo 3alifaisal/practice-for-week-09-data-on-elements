52e57f8f7ce02927e1315a814c14a94f2ec11ade
Ali Avada <110764863+3alifaisal@users.noreply.github.com>
2024-03-16 11:54:30 +0100

    learned to work with data attributes
    finished

--- data.js
+++ data.js
@@ -1,1 +1,18 @@
 // Your code here
+document.addEventListener("DOMContentLoaded",()=> {
+    const addButton = document.querySelector("#add");
+    const shoppingListUl = document.querySelector("#shopping-list");
+
+    console.log(addButton);
+    addButton.addEventListener("click", () => {
+       
+        const liElement = document.createElement("li");
+        const inputtedName = document.querySelector("#name");
+        const inputtedOption = document.querySelector("#type");
+
+        liElement.innerText = `product Name: ${inputtedName.value}`
+        liElement.setAttribute("data-type", inputtedOption.value)
+        shoppingListUl.appendChild(liElement);
+        event.preventDefault(); 
+    })
+})
